eced60e0ea6dea204cf163feee5c011273252584
Rayhan Sheikh <rayhan.sheikh10@gmail.com>
2023-12-10 18:15:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
+import About from "./pages/about/About"
+
+
+export default function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  )
+}
+
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,31 @@
+
+const Home = () => {
+  return (
+    <div className="container mx-auto bg-[#f5f6fa] ">
+      <div className="flex gap-x-5">
+
+        <div className="bg-green-500 w-[255px]">
+          <div className="ml-10 py-5">
+            <h2 className="text-lg text-blue-500 font-bold">Employee</h2>
+            <p className="text-[12px] text-[#878CA8]">Management System</p>
+          </div>
+          <hr />
+          <div className="ml-10">
+            <div className="bg-blue-500 text-white w-max px-4 py-2">
+              <h2>Dashboard</h2>
+            </div>
+            <div>
+              <h2>Dashboard</h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full">
+          main
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
