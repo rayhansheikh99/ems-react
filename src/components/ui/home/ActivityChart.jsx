e305c7298dcda8067a750971b7e3f94091f4adbc
Rayhan Sheikh <rayhan.sheikh10@gmail.com>
2023-12-19 01:07:41 +0600

    hhfg

--- src/components/ui/home/ActivityChart.jsx
+++ src/components/ui/home/ActivityChart.jsx
@@ -1,51 +1,109 @@
-import { Bar } from 'react-chartjs-2';
-import {
-    Chart,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js/auto';
+// import { Bar } from 'react-chartjs-2';
+// import {
+//     Chart,
+//     CategoryScale,
+//     LinearScale,
+//     BarElement,
+//     Title,
+//     Tooltip,
+//     Legend,
+// } from 'chart.js/auto';
 import moment from 'moment';
 import { FiCalendar } from 'react-icons/fi';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+// Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            display: false,
-            position: 'top',
-        },
-        title: {
-            display: false,
-            text: 'Chart.js Bar Chart',
-        },
+// const options = {
+//     responsive: true,
+//     plugins: {
+//         legend: {
+//             display: false,
+//             position: 'top',
+//         },
+//         title: {
+//             display: false,
+//             text: 'Chart.js Bar Chart',
+//         },
+//     },
+// };
+
+// const labels = ['09 am', '10 am', '11 am', '12 pm', '01 pm', '02 pm', '03 pm', '04 pm', "05 pm"];
+
+// const data = {
+//     labels,
+//     datasets: [
+//         {
+//             label: 'Working',
+//             data: [35, 30, 48, 25, 70, 50, 30, 20, 55],
+//             backgroundColor: '#3D55CC',
+//             borderRadius: "3",
+//         },
+//         {
+//             label: 'Rest',
+//             data: [60, 35, 38, 35, 40, 40, 50, 10, 25],
+//             backgroundColor: '#BDC4E2',
+//             borderRadius: "3",
+//         },
+//     ],
+// };
+
+const data = [
+    {
+        name: '09 am',
+        uv: 35,
+        pv: 58,
+        amt: 2400,
     },
-};
-
-const labels = ['09 am', '10 am', '11 am', '12 pm', '01 pm', '02 pm', '03 pm', '04 pm', "05 pm"];
-
-const data = {
-    labels,
-    datasets: [
-        {
-            label: 'Working',
-            data: [35, 30, 48, 25, 70, 50, 30, 20, 55],
-            backgroundColor: '#3D55CC',
-            borderRadius: "3",
-        },
-        {
-            label: 'Rest',
-            data: [60, 35, 38, 35, 40, 40, 50, 10, 25],
-            backgroundColor: '#BDC4E2',
-            borderRadius: "3",
-        },
-    ],
-};
+    {
+        name: '10 am',
+        uv: 30,
+        pv: 35,
+        amt: 2210,
+    },
+    {
+        name: '11 am',
+        uv: 47,
+        pv: 43,
+        amt: 2290,
+    },
+    {
+        name: '12 pm',
+        uv: 35,
+        pv: 25,
+        amt: 2000,
+    },
+    {
+        name: '01 pm',
+        uv: 18,
+        pv: 23,
+        amt: 2181,
+    },
+    {
+        name: '02 pm',
+        uv: 10,
+        pv: 5,
+        amt: 2500,
+    },
+    {
+        name: '03 pm',
+        uv: 27,
+        pv: 38,
+        amt: 2100,
+    },
+    {
+        name: '04 pm',
+        uv: 50,
+        pv: 60,
+        amt: 2100,
+    },
+    {
+        name: '05 pm',
+        uv: 40,
+        pv: 50,
+        amt: 2100,
+    },
+];
 
 const Date = moment().format('Do MMMM');
 
@@ -59,9 +117,21 @@ const ActivityChart = () => {
                     <p>{Date}</p>
                 </div>
             </div>
-            
-                <Bar options={options} data={data} />
-           
+
+            <div className="w-full bg-white p-5 h-80">
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart width={730} height={250} data={data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="pv" fill="#3D55CC" />
+                        <Bar dataKey="uv" fill="#BDC4E2" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+
         </div>
     )
 }
