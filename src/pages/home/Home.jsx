3e31b7f508aa7c743bab93480c206aa2683986cc
Rayhan Sheikh <rayhan.sheikh10@gmail.com>
2024-01-17 00:55:25 +0600

    task and leader board

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import SideNav from "../../components/ui/home/SideNav"
 import TaskProgress from "../../components/ui/home/TaskProgress"
 import TopHeader from "../../components/ui/home/TopHeader"
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import user1 from "../../assets/user/icon.jpg"
 
 const Home = () => {
 
@@ -105,8 +106,102 @@ const Home = () => {
                   </div>
                 </div>
               </div>
-              <div className="bg-red-400 w-full max-w-[30%] h-full">
-                hello
+              <div className="w-full max-w-[30%] grid grid-cols-1">
+                <div className="flex flex-col gap-5 ">
+                  <div className="h-full w-full bg-white p-4 rounded-sm">
+                    <div className="flex flex-col gap-3 h-full w-full">
+                      <h1 className="mb-3 font-bold text-xl text-[#505673]">Daily Task (3)</h1>
+                      <div className="grid grid-cols-2 gap-3 h-full w-full">
+                        <div className="bg-[#F5F6FA] relative">
+                          <p className="absolute top-3 left-3">Add new</p>
+                        </div>
+                        <div className="bg-[#D5DBF7] relative">
+                          <p className="absolute top-3 left-3">Add new</p>
+                        </div>
+                        <div className="bg-[#f7f3e3] relative">
+                          <p className="absolute top-3 left-3">Add new</p>
+                        </div>
+                        <div className="bg-[#dfedda] relative">
+                          <p className="absolute top-3 left-3">Add new</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-white rounded-sm">
+                    <div className="h-full w-full p-4">
+                      <div className="flex flex-col gap-3 h-full w-full">
+                        <h1 className="mb-3 font-bold text-xl text-[#505673]">Leader Board</h1>
+                        <div className="grid grid-cols-1 gap-3">
+                          <div className="flex items-center justify-between gap-x-2">
+                            <div className="flex items-center gap-x-3">
+                              <img src={user1} alt="" className="w-10 h-10" />
+                              <div>
+                                <h3 className="font-bold">Rayhan Sheikh</h3>
+                                <p className="text-xs text-[#878CA8]">Frontend Developer</p>
+                              </div>
+                            </div>
+                            <div className="text-[#7AAD69]">
+                              <h3 className="font-bold">17 Points</h3>
+                              <p className="text-[12px] text-right">+20%</p>
+                            </div>
+                          </div>
+                          <div className="flex items-center justify-between gap-x-2">
+                            <div className="flex items-center gap-x-3">
+                              <img src={user1} alt="" className="w-10 h-10" />
+                              <div>
+                                <h3 className="font-bold">Rayhan Sheikh</h3>
+                                <p className="text-xs text-[#878CA8]">Frontend Developer</p>
+                              </div>
+                            </div>
+                            <div className="text-[#7AAD69]">
+                              <h3 className="font-bold">17 Points</h3>
+                              <p className="text-[12px] text-right">+20%</p>
+                            </div>
+                          </div>
+                          <div className="flex items-center justify-between gap-x-2">
+                            <div className="flex items-center gap-x-3">
+                              <img src={user1} alt="" className="w-10 h-10" />
+                              <div>
+                                <h3 className="font-bold">Rayhan Sheikh</h3>
+                                <p className="text-xs text-[#878CA8]">Frontend Developer</p>
+                              </div>
+                            </div>
+                            <div className="text-[#7AAD69]">
+                              <h3 className="font-bold">17 Points</h3>
+                              <p className="text-[12px] text-right">+20%</p>
+                            </div>
+                          </div>
+                          <div className="flex items-center justify-between gap-x-2">
+                            <div className="flex items-center gap-x-3">
+                              <img src={user1} alt="" className="w-10 h-10" />
+                              <div>
+                                <h3 className="font-bold">Rayhan Sheikh</h3>
+                                <p className="text-xs text-[#878CA8]">Frontend Developer</p>
+                              </div>
+                            </div>
+                            <div className="text-[#7AAD69]">
+                              <h3 className="font-bold">17 Points</h3>
+                              <p className="text-[12px] text-right">+20%</p>
+                            </div>
+                          </div>
+                          <div className="flex items-center justify-between gap-x-2">
+                            <div className="flex items-center gap-x-3">
+                              <img src={user1} alt="" className="w-10 h-10" />
+                              <div>
+                                <h3 className="font-bold">Rayhan Sheikh</h3>
+                                <p className="text-xs text-[#878CA8]">Frontend Developer</p>
+                              </div>
+                            </div>
+                            <div className="text-[#7AAD69]">
+                              <h3 className="font-bold">17 Points</h3>
+                              <p className="text-[12px] text-right">+20%</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div>
